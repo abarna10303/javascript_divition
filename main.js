ecc8cfe58d3f75a2a90c180eb3ecb3a5afe40f4f
abarna10303 <abarnas361@gmail.com>
2022-12-13 17:08:05 +0530

    Divisition

---
+++ main.js
@@ -0,0 +1,7 @@
+/*Divition PROGRAM*/
+var num1,num2,divition;
+num1=parseInt(prompt("Enter the First Number"));
+num2=parseInt(prompt("Enter the second Number"));
+divition=num1/num2;
+console.log("Divition of Two number is : " +divition);
+
